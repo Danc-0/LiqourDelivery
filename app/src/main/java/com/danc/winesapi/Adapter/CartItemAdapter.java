841b8dcc37120657b6cfdf8aa4126c641adfc672
Danc-0 <lunjaludaniel@gmail.com>
2020-09-18 15:23:58 +0300

    CartActivity upgrades on Performance

--- app/src/main/java/com/danc/winesapi/Adapter/CartItemAdapter.java
+++ app/src/main/java/com/danc/winesapi/Adapter/CartItemAdapter.java
@@ -16,20 +16,22 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.danc.winesapi.R;
 import com.squareup.picasso.Picasso;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 
-public class CartItemAdapter extends RecyclerView.Adapter<CartItemAdapter.CartViewHolder>  {
+public class CartItemAdapter extends RecyclerView.Adapter<CartItemAdapter.CartViewHolder> {
 
     private static final String TAG = "CartItemAdapter";
     private Context context;
     private ArrayList title, imageUrl, price, originalPrice, quantity;
 
-   public  CartItemAdapter(Context context,
+    public CartItemAdapter(Context context,
                            ArrayList title,
                            ArrayList imageUrl1,
                            ArrayList price,
                            ArrayList initialPrice,
-                           ArrayList quantity){
+                           ArrayList quantity) {
         this.context = context;
         this.title = title;
         this.imageUrl = imageUrl1;
@@ -55,8 +57,10 @@ public class CartItemAdapter extends RecyclerView.Adapter<CartItemAdapter.CartVi
         holder.price.setText(String.valueOf(price.get(position)));
         holder.initialPrice.setText(String.valueOf(originalPrice.get(position)));
         holder.quantity.setText(String.valueOf(quantity.get(position)));
-
         Picasso.get().load(String.valueOf(imageUrl.get(position))).into(holder.image);
+
+//        getTotalCost(Integer.parseInt(String.valueOf(quantity.get(position))), Integer.parseInt(String.valueOf(price.get(position))));
+
     }
 
     @Override
@@ -64,11 +68,14 @@ public class CartItemAdapter extends RecyclerView.Adapter<CartItemAdapter.CartVi
         return title.size();
     }
 
-    public class CartViewHolder extends RecyclerView.ViewHolder{
+    public class CartViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        TextView title, price, initialPrice, quantity;
+        TextView title, price, initialPrice, quantity, totalPrice;
         ImageView image;
         RelativeLayout increaseQuantity, decreaseQuantity;
+
+        int currentQty;
+        int pricePerPiece;
 
         public CartViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -78,10 +85,41 @@ public class CartItemAdapter extends RecyclerView.Adapter<CartItemAdapter.CartVi
             price = itemView.findViewById(R.id.price);
             initialPrice = itemView.findViewById(R.id.original_price);
             quantity = itemView.findViewById(R.id.quantity);
+//            totalPrice = itemView.findViewById(R.id.total_amount);
 
             increaseQuantity = itemView.findViewById(R.id.increase_quantity);
             decreaseQuantity = itemView.findViewById(R.id.decrease_quantity);
 
+            increaseQuantity.setOnClickListener(this);
+            decreaseQuantity.setOnClickListener(this);
+
+        }
+
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.increase_quantity:
+                    currentQty = Integer.parseInt(quantity.getText().toString());
+                    currentQty++;
+                    quantity.setText(String.valueOf(currentQty));
+                    break;
+
+                case R.id.decrease_quantity:
+                    currentQty = Integer.parseInt(quantity.getText().toString());
+
+                    if (currentQty <= 1) {
+                        quantity.setText(String.valueOf(currentQty));
+                    } else {
+                        currentQty--;
+                        quantity.setText(String.valueOf(currentQty));
+                        break;
+                    }
+            }
         }
     }
+
+//    public void getTotalCost(int s1, int s2){
+//        int calculated = s1 * s2;
+//        totalPrice.setText(String.valueOf(calculated));
+//    }
 }
--- app/src/main/java/com/danc/winesapi/ProductActivity.java
+++ app/src/main/java/com/danc/winesapi/ProductActivity.java
@@ -9,8 +9,13 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.danc.winesapi.Adapter.ProductAdapter;
 import com.danc.winesapi.Interfaces.ApiClient;
@@ -22,7 +27,7 @@ import java.util.List;
 
 import retrofit2.Retrofit;
 
-public class ProductActivity extends AppCompatActivity {
+public class ProductActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "MainActivity";
 
     Retrofit retrofit;
@@ -34,6 +39,7 @@ public class ProductActivity extends AppCompatActivity {
     CartItemOpenHelper mDb;
 
     public TextView item_count;
+    LinearLayout Cart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,8 +48,13 @@ public class ProductActivity extends AppCompatActivity {
 
         item_count = findViewById(R.id.item_count);
         recyclerView = findViewById(R.id.recycler_view);
+        Cart = findViewById(R.id.cart);
+        Cart.setOnClickListener(this);
+
+        mDb = new CartItemOpenHelper(this);
 
         init();
+        getAllData();
 
         LocalBroadcastManager.getInstance(this).registerReceiver(mReceiver, new IntentFilter("INTENT_NAME"));
 
@@ -67,6 +78,32 @@ public class ProductActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        getAllData();
         LocalBroadcastManager.getInstance(this).registerReceiver(mReceiver, new IntentFilter("INTENT_NAME"));
+    }
+
+    void getAllData() {
+        Cursor cursor = mDb.readAllData();
+        if (cursor.getCount() == 0) {
+            item_count.setVisibility(View.INVISIBLE);
+            Toast.makeText(this, "Failed to Fetch the data", Toast.LENGTH_SHORT).show();
+
+        } else {
+            while (cursor.moveToNext()) {
+                Log.d(TAG, "getAllData: Number of items " + cursor.getCount());
+                Toast.makeText(this, "Number of Items " + cursor.getCount(), Toast.LENGTH_SHORT).show();
+                item_count.setText(String.valueOf(cursor.getCount()));
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.cart:
+            Intent intent = new Intent(this, CartActivity.class);
+            startActivity(intent);
+            break;
+        }
     }
 }
